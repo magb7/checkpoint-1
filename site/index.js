77523e71e51c16fb66100b9f71ece819e143b126
magb7 <margaux.bedu@gmail.com>
2020-04-03 16:15:46 +0200

    add empty glass on click

--- site/index.js
+++ site/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
   const whiskyGlass = document.querySelector('#glass');
   whiskyGlass.addEventListener('click', () => {
-    
+    whiskyGlass.src = 'image/empty_whisky.png';
   });
 });
